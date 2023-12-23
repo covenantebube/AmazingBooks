b41082f20db5bf4e375f4110b1a8078a86676c33
covenantebube <covenantebube1@gmail.com>
2023-12-23 14:30:40 +0100

    Add project files.

---
+++ AmazingBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using AmazingBooks.DataAccess.Data;
+using AmazingBooks.DataAccess.Repository.IRepository;
+using AmazingBooks.Models;
+
+namespace AmazingBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ AmazingBooks.Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AmazingBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
